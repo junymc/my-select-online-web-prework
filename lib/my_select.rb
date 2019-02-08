ff169b07add67e4126751d06b046da24d7b5104e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-08 00:46:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,14 +2,15 @@ def my_select(collection)
  i = 0
  array = []
   while i < collection.length
-   array << yield(collection[i])
+    if yield(array[i])
+   array << collection[i]
    i = i + 1
  end
  array# your code here!
 end
 
 
-my_select([1,2,3,4]).select do
-  |n| n == even
-  puts n
-end
+# my_select([1,2,3,4]).select do
+#   |n| n == even
+#   puts n
+# end
